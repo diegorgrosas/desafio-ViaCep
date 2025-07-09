5765dd3debede9d6af81ca9054542bff963de904
Diego Rosas <diegorosas19@gmail.com>
2025-07-09 18:29:19 -0300

    Programa simples para fazer requisição ViaCep

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um cep para buscar informações: ");
+        String busca = leitura.nextLine();
+
+        String endereco = "https://viacep.com.br/ws/" + busca + "/json";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco))
+                .header("Accept", "application/vnd.github.v3+json")
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+        System.out.println(json);
+        System.out.println("Busca finalizada com sucesso!");
+
     }
 }
