be50bbcfecec501836d82866162c078408b1bd52
Diego Rosas <diegorosas19@gmail.com>
2025-07-09 21:00:08 -0300

    Adicionando menu e loop while

--- src/Main.java
+++ src/Main.java
@@ -8,20 +8,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner leitura = new Scanner(System.in);
-        System.out.println("Digite um cep para buscar informações: ");
-        String busca = leitura.nextLine();
+        String busca = "";
 
-        String endereco = "https://viacep.com.br/ws/" + busca + "/json";
+        System.out.println("==============================================================================");
+        System.out.println("                   Bem vindos ao Aplicativo de busca ViaCep!                  ");
+        System.out.println("==============================================================================");
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco))
-                .header("Accept", "application/vnd.github.v3+json")
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        while (!busca.equalsIgnoreCase("sair")) {
 
-        String json = response.body();
-        System.out.println(json);
-        System.out.println("Busca finalizada com sucesso!");
+            System.out.print("Digite um cep para buscar informações: ");
+            busca = leitura.nextLine();
+
+            if (busca.equalsIgnoreCase("sair")) {
+                break;
+            }
+
+            String endereco = "https://viacep.com.br/ws/" + busca + "/json";
+            System.out.println(endereco);
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco))
+                    .header("Accept", "application/vnd.github.v3+json")
+                    .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            System.out.println(json);
+            System.out.println("Busca finalizada com sucesso!");
+        }
+
 
     }
 }
